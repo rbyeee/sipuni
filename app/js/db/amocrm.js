a08e4530aa1b7394e42f340df67ab2467c92b882
Egor Novikov <42256950+rbyea@users.noreply.github.com>
2026-02-25 01:42:09 +0300

    25.02

--- app/js/db/amocrm.js
+++ app/js/db/amocrm.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
 	$(".blog__height").matchHeight()
+
+	$(".tab-amo-item").on("click", function (e) {
+		e.preventDefault()
+		const $btn = $(this)
+
+		if ($btn.hasClass("active")) return
+
+		// Переключаем кнопки
+		$(".tab-amo-item").removeClass("active")
+		$btn.addClass("active")
+
+		// Переключаем картинки по индексу
+		const index = $(".tab-amo-item").index($btn)
+		$(".tab-amo-images img")
+			.removeClass("is-active")
+			.eq(index)
+			.addClass("is-active")
+	})
 })
